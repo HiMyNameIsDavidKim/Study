c0950335c6b1c3072f1790cc7ea0dc2c9d9b371f
David Kim <rkfka1401@gmail.com>
2022-09-15 18:25:36 +0900

    9/15 study update

--- 1Python/0나도코딩(기본)/7함수.py
+++ 1Python/0나도코딩(기본)/7함수.py
@@ -7,6 +7,39 @@ def open_account() :
 open_account()
 
 #함수 공부2(전달값과 반환값)
+def deposit(balance, money) :
+    print("Deposit is completed. now balance is {0}".format(balance + money))
+    return balance + money
+
+account1 = 0
+account1 = deposit(account1, 1000)
+
+def withdraw(balance, money) :
+    if balance >- money :
+        print("Withdraw is completed. now balance is {0}".format(balance - money))
+        return balance - money
+    else :
+        print("Withdraw is failed. now balance is {0}.".format(balance))    
+        return balance
+
+account2 = 0
+account2 = deposit(account2, 1000)
+account2 = withdraw(account2, 500)
+
+def withdraw_night(balance, money) : 
+    commission = 100
+    print("Commission is {0}, now balance is {1}".format(commission, balance - money - commission))
+    return commission, balance - money - commission
+
+account3 = 2000
+account3 = withdraw_night(account3, 1000)
+
+
+
+
+
+
+
 
 
 
