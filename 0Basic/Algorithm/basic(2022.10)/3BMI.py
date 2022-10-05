d007a34a6a8657fce3ea6fc25d41e6b7d6290c2c
David Kim <rkfka1401@gmail.com>
2022-10-05 17:58:42 +0900

    10/5 study update

--- 0Basic/Algorithm/basic(2022.10)/3BMI.py
+++ 0Basic/Algorithm/basic(2022.10)/3BMI.py
@@ -1,3 +1,4 @@
+#상단에 문제 써놓고 시작하는 습관 필요함.
 """
 키와 몸무게를 입력받아서 비만도를 측정하는 프로그램을 완성하시오.
 bmi 지수를 구하는 공식은 다음과 같다.
@@ -24,29 +25,25 @@ class Solution :
         title = "[Body Mass Index]"
         aster = "*"*30
         bmi = (kg) / (m * m)
-        index = ""
-        if bmi >= 35 : 
-            index = "고도 비만"
-        elif bmi >= 30 : 
-            index = "중도 비만"
-        elif bmi >= 25 : 
-            index = "경도 비만"
-        elif bmi >= 23 : 
-            index = "과체중"
-        elif bmi >= 18.5 : 
-            index = "정상"
-        elif bmi < 18.5 : 
-            index = "저체중"
+        index = "" #자연어를 출력할 때는 항상 빈걸로 선언 먼저 해줘야함.
+        if bmi >= 35 : index = "고도 비만" #if문에서 짧으면 붙여서 써도 무관함.
+        elif bmi >= 30 : index = "중도 비만"
+        elif bmi >= 25 : index = "경도 비만"
+        elif bmi >= 23 : index = "과체중"
+        elif bmi >= 18.5 : index = "정상"
+        elif bmi < 18.5 : index = "저체중"
         schema = "이름 키(cm) 몸무게(kg) 비만도"
-        values = f"{name} {round(m*100)} {kg} {index}"
+        values = f"{name} {round(m*100)} {kg} {index}" #f스트링 쓰는 연습 해야함. 속도도 제일 빠르고 직관적임.
         return f"{aster}\n{title}\n{aster}\n{schema}\n{aster}\n{values}\n{aster}"
 
 if __name__ == "__main__" :
     solution = Solution()
     name = input("Please input your name : ")
-    m = float(input("Please input your height : "))
-    kg = int(input("Please input your weight : "))
+    m = float(input("Please input your height(m) : "))
+    kg = int(input("Please input your weight(kg) : "))
     print(solution.solution(name, m, kg))
+
+
 
 
 
