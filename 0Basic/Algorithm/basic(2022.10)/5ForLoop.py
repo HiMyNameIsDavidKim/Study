82b28618414ab13a145c386bf740fb3717fa5810
David Kim <rkfka1401@gmail.com>
2022-10-06 17:11:12 +0900

    10/6 study

--- 0Basic/Algorithm/basic(2022.10)/5ForLoop.py
+++ 0Basic/Algorithm/basic(2022.10)/5ForLoop.py
@@ -1,10 +1,11 @@
 def solution() :
     money = 1660
     count = 0
+    rest = money
     a = [500, 100, 50, 10]
     for i in a :
-        count = money // i
-        
+        count = rest // i
+        rest = money % i
         print(f"{i}원짜리 {count}개")
 
 if __name__ == "__main__" :
