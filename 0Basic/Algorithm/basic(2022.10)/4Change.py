65203b0b722e34554ac1cc15362637bc4915fbca
David Kim <rkfka1401@gmail.com>
2022-10-06 17:51:10 +0900

    10/6 study update

--- 0Basic/Algorithm/basic(2022.10)/4Change.py
+++ 0Basic/Algorithm/basic(2022.10)/4Change.py
@@ -2,21 +2,21 @@
  * 금액을 입력받은 후 우리나라 화폐종류별로 해당 갯수를 표기하는 프로그램
     * [요구사항] 금융업을 하는 고객사로부터 프로그램 개발요청이 들어왔습니다.
     * 금액을 입력받은 후 우리나라 화폐종류별로 해당 갯수를 표기하는 프로그램입니다.
-    * 예를들어, 125,520 원을 입력하면 화면에 이렇게 보이게 하면 됩니다.
+    * 예를들어, 126,520 원을 입력하면 화면에 이렇게 보이게 하면 됩니다.
     * 표시하고 10원미만은 절삭
       ******************************************************
          요청금액 : 126520 원
-         5만원 : 2매
-         1만원 : 2매
-         5천원 : 1매
-         1천원 : 1매
-         5백원 : 1개
-         백원 : 0개
-         오십원 : 0개
-         십원 : 2개
+         50000원 : 2매
+         10000원 : 2매
+         5000원 : 1매
+         1000원 : 1매
+         500원 : 1개
+         100원 : 0개
+         50원 : 0개
+         10원 : 2개
       ********************************************************
 """
-#Greedy 개념을 반영하지 않은 코드
+# #Greedy 개념을 반영하지 않은 코드
 # class Solution :
 #     def solution(self, money) :
 #         title = " ### Money Changer ### "
@@ -50,28 +50,49 @@
 #     print(solution.solution(money))
 
 
-#Greedy 개념을 반영한 코드
+# #Greedy 개념을 반영한 코드
+# class Solution :
+#     def solution(self, money) :
+#         title = " ### Money Changer ### "
+#         aster = "*"*30
+#         answer = f"요청금액 : {money:,} 원"
+#         unit = [money+1, 50000, 10000, 5000, 1000, 500, 100, 50, 10]
+#         unit2 = ["5만", "1만", "5천", "1천", "5백", "백", "오십", "십"]
+#         result = (
+#             f"{title}\n{aster}\n{answer}"
+#         )
+#         for i in range(0,8) : #0~7까지 진행
+#             result = result + f"\n{unit2[i]}원 : {money % unit[i] // unit[i+1]}"
+#         return result
+
+# if __name__ == "__main__" :
+#     solution = Solution()
+#     money = int(input("Please input money : "))
+#     print(solution.solution(money))
+
+
+#더 간결한 버전
 class Solution :
     def solution(self, money) :
         title = " ### Money Changer ### "
         aster = "*"*30
         answer = f"요청금액 : {money:,} 원"
-        unit = [money+1, 50000, 10000, 5000, 1000, 500, 100, 50, 10]
-        unit2 = ["5만", "1만", "5천", "1천", "5백", "백", "오십", "십"]
+        unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+        count = 0
         result = (
-            f"{title}\n{aster}\n{answer}"
+            f"{title}\n{aster}\n{answer}\n"
         )
-        for i in range(0,8) : #0~7까지 진행
-            result = result + f"\n{unit2[i]}원 : {money % unit[i] // unit[i+1]}"
+        for i in unit :
+            count = money // i
+            money %= i
+            result += f"{i}원 : {count}\n"
+        result += aster
         return result
 
 if __name__ == "__main__" :
     solution = Solution()
     money = int(input("Please input money : "))
     print(solution.solution(money))
-
-
-
 
 
 
--- 0Basic/Algorithm/basic(2022.10)/5ForLoop.py
+++ 0Basic/Algorithm/basic(2022.10)/5ForLoop.py
@@ -1,18 +1,14 @@
 def solution() :
     money = 1660
     count = 0
-    rest = money
     a = [500, 100, 50, 10]
     for i in a :
-        count = rest // i
-        rest = money % i
+        count = money // i
+        money %= i
         print(f"{i}원짜리 {count}개")
 
 if __name__ == "__main__" :
     solution()
-
-
-
 
 
 
