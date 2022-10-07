bd719c205e7cb899ebc9c39a9e7d1659d017cebe
David Kim <rkfka1401@gmail.com>
2022-10-07 17:10:01 +0900

    10/7 study update

---
+++ 0Basic/Algorithm/basic(2022.10)/6BubbleSort.py
@@ -0,0 +1,35 @@
+import random
+
+class Solution :
+    def solution(self) :
+        keys = ["1st", "2nd", "3rd", "4th", "5th"]
+        dc = {}
+        for i in keys : 
+            dc[i] = random.randint(1,100)
+
+        print(f" ### Bubble Sort ### ")
+        print(f"*"*30)
+
+        for k,v in dc.items() : 
+            print(f"{k} : {v}")
+
+        print(f"*"*30)
+
+if __name__ == "__main__" : #리턴 없이 간소화된 버전
+    solution = Solution()
+    solution.solution() #리턴 지우고 프린트만 지우면 됨.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
