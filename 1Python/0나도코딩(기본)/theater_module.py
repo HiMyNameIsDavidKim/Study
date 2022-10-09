28ffd7bfae9b809ab695464ea0ba85618133440e
David Kim <rkfka1401@gmail.com>
2022-10-09 23:03:26 +0900

    10/9 study update

---
+++ 1Python/0나도코딩(기본)/theater_module.py
@@ -0,0 +1,20 @@
+def price(people):
+    print(f"{people}ea ticket price is {people * 10000}Won.")
+
+def price_morning(people):
+    print(f"{people}ea morning ticket price is {people * 6000}Won.")
+
+def price_soldier(people):
+    print(f"{people}ea soldier ticket price is {people * 4000}Won.")
+
+
+
+
+
+
+
+
+
+
+
+    
