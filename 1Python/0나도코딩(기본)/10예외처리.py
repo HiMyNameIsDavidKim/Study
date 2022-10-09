77d38c89df14dec651cf13c8686896269f0e2435
David Kim <rkfka1401@gmail.com>
2022-10-09 22:52:03 +0900

    10/9 study update

--- 1Python/0나도코딩(기본)/10예외처리.py
+++ 1Python/0나도코딩(기본)/10예외처리.py
@@ -30,7 +30,76 @@ except ValueError :
 
 
 #예외처리 공부3(사용자 정의 예외처리)
+class BigNumberError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
+    
+try :
+    print("Calculator for only 1 length number.")
+    num1 = int(input("Please input first number. : "))
+    num2 = int(input("Please input second number. : "))
+    if num1 >= 10 or num2 >= 10 :
+        raise BigNumberError("Input : {0}, {1}".format(num1, num2)) #해당 에러로 지정하겠다.
+    print("{0}/{1} = {2}".format(num1, num2, int(num1/num2)))
+except ValueError :
+    print("Error! strange number inputed. please input only 1 length number.")
+except BigNumberError:
+    print("Error! strange number inputed. please input only 1 length number.")
 
+
+#예외처리 공부4(Finally)
+#에러가 나든 안나든 트라이 구문이 끝날때 무조건 실행되는 명령을 지정할 수 있음.
+class BigNumberError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
+    
+try :
+    print("Calculator for only 1 length number.")
+    num1 = int(input("Please input first number. : "))
+    num2 = int(input("Please input second number. : "))
+    if num1 >= 10 or num2 >= 10 :
+        raise BigNumberError("Input : {0}, {1}".format(num1, num2)) #해당 에러로 지정하겠다.
+    print("{0}/{1} = {2}".format(num1, num2, int(num1/num2)))
+except ValueError :
+    print("Error! strange number inputed. please input only 1 length number.")
+except BigNumberError:
+    print("Error! strange number inputed. please input only 1 length number.")
+finally:
+    print("Thank you to use our calculator.")
+
+
+#Quiz 풀이
+chicken = 10
+waiting = 1
+
+class SoldOutError(Exception):
+    pass
+
+while(True):
+    try:
+        print("[left chicken : {0}]".format(chicken))
+        order =int(input("How many do you order chicken? : "))
+
+        if order > chicken:
+            print("chicken is not enough now.")
+        elif order <= 0:
+            raise ValueError
+        else:
+            print("[Waiting number {0}] {1}ea chicken order is completed.".format(waiting, order))
+            waiting += 1
+            chicken -= order
+        if chicken == 0:
+            raise SoldOutError
+
+    except ValueError:
+        print("Error! strange thing is inputed.")
+    except SoldOutError:
+        print("We don't take order any more.")
+        break
 
 
 
