3142fa17ed90ec00d5767d980728a6580cc0f08c
David Kim <rkfka1401@gmail.com>
2022-10-12 17:35:56 +0900

    10/12 study update

---
+++ 0Basic/Algorithm/basic(Java)/Algorithm/Sort/3BubbleSort.java
@@ -0,0 +1,13 @@
+package Algorithm.Sort;
+
+class BubbleSort{
+    public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
+        System.out.println(bubbleSort.solution());
+    }
+
+    String solution(){
+        String title = "### Bubble Sort ###";
+        return title;
+    }
+}
