276f5fd8bbc7486cea6e22e1cfe8809354aa2913
David Kim <rkfka1401@gmail.com>
2022-10-12 17:53:44 +0900

    10/12 study update

--- 0Basic/Algorithm/basic(Java)/Algorithm/Sort/3BubbleSort.java
+++ 0Basic/Algorithm/basic(Java)/Algorithm/Sort/3BubbleSort.java
@@ -1,13 +1,33 @@
+/**
+간소화 버전 숙지
 package Algorithm.Sort;
 
 class BubbleSort{
     public static void main(String[] args) {
-        BubbleSort bubbleSort = new BubbleSort();
-        System.out.println(bubbleSort.solution());
+        new BubbleSort().solution();
     }
 
-    String solution(){
-        String title = "### Bubble Sort ###";
-        return title;
+    void solution(){
+        System.out.println("### Bubble Sort ###");
     }
 }
+*/
+//내림차순 정렬 할때 그 정렬임.
+
+
+
+package Algorithm.Sort;
+import java.util.*;;
+
+class BubbleSort{
+    public static void main(String[] args) {
+        new BubbleSort().solution();
+    }
+
+    void solution(){
+        int randNumber = (int)(Math.random()*10)+1; //1~10까지 랜덤
+        System.out.println("Random Number : "+randNumber);
+    }
+
+}
+
