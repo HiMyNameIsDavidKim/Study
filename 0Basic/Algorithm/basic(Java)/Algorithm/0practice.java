3b6694e5a12cc0d8866249236bbc4f047a6b27b3
David Kim <rkfka1401@gmail.com>
2022-10-14 16:43:12 +0900

    10/14 study update

--- 0Basic/Algorithm/basic(Java)/Algorithm/0practice.java
+++ 0Basic/Algorithm/basic(Java)/Algorithm/0practice.java
@@ -5,27 +5,39 @@ class Solution0{
     public static void main(String[] args) {
         new Solution0().solution();
     }
+    
     void solution(){
-        int[] arr = creatArray();
+        int[] arr = createArray();
         arr = sortArray(arr);
         printArray(arr);
     }
-    int[] creatArray(){
+    int[] createArray(){
         int[] arr = new int[10];
-        for(int i=0; i<arr.length; i++){
+        for(int i=0;i<arr.length;i++){
             arr[i] = (int)((Math.random()*10)+1);
-            for(int j=0; j<i; j++){
-                if(arr[j]==arr[i]){i--;}
+            for(int j=0;j<i;j++){
+                if(arr[j] == arr[i]){i--;}
             }
         }
         return arr;
     }
     int[] sortArray(int[] arr){
-        
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j] > arr[j+1]){
+                    int t = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=t;
+                }
+            }
+
+        }
         return arr;
     }
     void printArray(int[] arr){
-        System.out.println(Arrays.toString(arr));
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
     }
 }
 
