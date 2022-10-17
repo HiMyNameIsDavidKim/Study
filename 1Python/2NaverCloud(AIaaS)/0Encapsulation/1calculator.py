40e74e35fc64d4459692d99ec9cc91b190459268
David Kim <rkfka1401@gmail.com>
2022-10-17 16:19:21 +0900

    10/17 study update

--- 1Python/2NaverCloud(AIaaS)/0Encapsulation/1calculator.py
+++ 1Python/2NaverCloud(AIaaS)/0Encapsulation/1calculator.py
@@ -1,7 +1,20 @@
-class Calculator: #클래스
-    def set_data(): #메서드
-        pass
+class Calculator(object): #클래스, (object)를 써줌으로써 클래스 객체가 된다.
+    def __init__(self, num1, op, num2):
+        self.num1 = num1 #디스크에 있는 num1에 메모리에 있는 num1을 입력해라.
+        self.op = op
+        self.num2 = num2
+
+    def calc(self): #메서드
+        if self.op == "+": print(f"{self.num1} {self.op} {self.num2} = {self.num1+self.num2}") #클래스 안에서는 언제나 셀프로 해줘야함.
+        elif self.op == "-": print(f"{self.num1} {self.op} {self.num2} = {self.num1-self.num2}")
+        elif self.op == "*": print(f"{self.num1} {self.op} {self.num2} = {self.num1*self.num2}")
+        elif self.op == "/": print(f"{self.num1} {self.op} {self.num2} = {self.num1/self.num2}")
+        elif self.op == "%": print(f"{self.num1} {self.op} {self.num2} = {self.num1%self.num2}")
 
 if __name__ == "__main__":
-    calculator = Calculator()
-    print(calculator.set_data())
+    num1 = int(input("First Number : "))
+    op = input("Please choose Operator(+,-,*,/,%) : ")
+    num2 = int(input("Second Number : "))
+    calculator = Calculator(num1, op, num2)
+    calculator.calc()
+    
