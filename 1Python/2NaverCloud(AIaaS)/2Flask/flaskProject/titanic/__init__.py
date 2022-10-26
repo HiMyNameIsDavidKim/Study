3f158a6fff7a944ee507170ddcdad802c71871bf
David Kim <rkfka1401@gmail.com>
2022-10-26 18:02:12 +0900

    10/26 study update

--- 1Python/2NaverCloud(AIaaS)/2Flask/flaskProject/titanic/__init__.py
+++ 1Python/2NaverCloud(AIaaS)/2Flask/flaskProject/titanic/__init__.py
@@ -2,19 +2,20 @@ from titanic.template import Plot
 from titanic.views import TitanicController
 from util.common import Common
 
-api = TitanicController()
-while True:
-    menu = Common.menu(["종료", "시각화", "모델링", "머신러닝", "배포"])
-    if menu == "0":
-        print(" ### 종료 ### ")
-        break
-    elif menu == "1":
-        print(" ### 시각화 ### ")
-        plot = Plot("train.csv")
-        plot.draw_survived()
-    elif menu == "2":
-        print(" ### 모델링 ### ")
-    elif menu == "3":
-        print(" ### 머신러닝 ### ")
-    else:
-        print(" ### 해당 메뉴 없음 ### ")
+if __name__ == "__main__":
+    api = TitanicController()
+    while True:
+        menu = Common.menu(["종료", "시각화", "모델링", "머신러닝", "배포"])
+        if menu == "0":
+            print(" ### 종료 ### ")
+            break
+        elif menu == "1":
+            print(" ### 시각화 ### ")
+            plot = Plot("train.csv")
+            plot.draw_sex()
+        elif menu == "2":
+            print(" ### 모델링 ### ")
+        elif menu == "3":
+            print(" ### 머신러닝 ### ")
+        else:
+            print(" ### 해당 메뉴 없음 ### ")
--- 1Python/2NaverCloud(AIaaS)/2Flask/flaskProject/titanic/template.py
+++ 1Python/2NaverCloud(AIaaS)/2Flask/flaskProject/titanic/template.py
@@ -20,9 +20,25 @@ class Plot(object):
     def draw_survived(self):
         this = self.entry
         f, ax = plt.subplots(1, 2, figsize=(18, 8)) #한 화면에 2개 그래프 그린다.
-        this['Survived'].value_counts().plot.pie(explode=[0, 0.1], autopct='%1.1f%%', ax=ax[0], shadow=True)
+        this['Survived'].value_counts().plot.pie(explode=[0, 0.1], autopct='%1.1f%%', ax=ax[0], shadow=True) #파이차트를 그린다.
         ax[0].set_title('0.사망자 vs 1.생존자')
         ax[0].set_ylabel('')
         ax[1].set_title('0.사망자 vs 1.생존자')
         sns.countplot(x='Survived', data=this, ax=ax[1])
+        plt.show()
+
+    def draw_pclass(self):
+        this = self.entry
+        this["생존결과"] = this["Survived"].replace(0, "사망자").replace(1, "생존자")
+        this["좌석등급"] = this["Pclass"].replace(1, "1등석").replace(2, "2등석").replace(3, "3등석")
+        sns.countplot(data=this, x="좌석등급", hue="생존결과")
+        plt.show()
+
+    def draw_sex(self):
+        this = self.entry
+        f, ax = plt.subplots(1, 2, figsize=(18, 8)) #한 화면에 2개 그래프 그린다.
+        this['Survived'][this['Sex']=='male'].value_counts().plot.pie(explode=[0, 0.1], autopct='%1.1f%%', ax=ax[0], shadow=True)
+        this['Survived'][this['Sex']=='female'].value_counts().plot.pie(explode=[0, 0.1], autopct='%1.1f%%', ax=ax[1], shadow=True)
+        ax[0].set_title('남성의 생존비율 [0.사망자 vs 1.생존자]')
+        ax[1].set_title('여성의 생존비율 [0.사망자 vs 1.생존자]')
         plt.show()
