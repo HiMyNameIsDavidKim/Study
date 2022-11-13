df69056194475e22746ca8a744585b7e0bf29a76
David Kim <rkfka1401@gmail.com>
2022-11-13 21:59:04 +0900

    11/13 study update

--- 1Python/Dojang/2Statements/unit22_2의제곱.py
+++ 1Python/Dojang/2Statements/unit22_2의제곱.py
@@ -1,4 +1,4 @@
 start, stop = map(int, input().split())
 
 for i in range(start, stop+1):
-    print(('Fizz' * (i%5==0) + 'Buzz' * (i%7==0)) or i)
+    print(f'{2**i}', end=' ')
---
+++ 1Python/Dojang/2Statements/unit23_지뢰찾기.py
@@ -0,0 +1,28 @@
+x, y = map(int, input().split())
+
+matrix = []
+for i in range(x):
+    matrix.append([])
+    n_row = input()
+    n_row = list(n_row)
+    for j in range(y):
+        if n_row[j] == '*': matrix[i].append(n_row[j])
+        elif n_row[j] == '.': matrix[i].append(0)
+
+for i in range(x):
+    for j in range(y):
+        if matrix[i][j] == '*':
+            for k in range(-1,2):
+                for w in range(-1,2):
+                    try :
+                        if matrix[i+k][j+w] == '*': pass
+                        elif matrix[i+k][j+w] != '*' and i+k>=0 and j+w>=0: matrix[i+k][j+w] += 1
+                        else: pass
+                    except IndexError: continue
+        else: continue
+
+print('\n[answer]\n')
+for i in range(x):
+    for j in range(y):
+        print(matrix[i][j], end='')
+    print()
