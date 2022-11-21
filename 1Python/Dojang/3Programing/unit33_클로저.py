781e050173a6aea3e670f71cdfbad47f7ff03246
David Kim <rkfka1401@gmail.com>
2022-11-22 00:49:33 +0900

    11/22 study update

--- 1Python/Dojang/3Programing/unit33_클로저.py
+++ 1Python/Dojang/3Programing/unit33_클로저.py
@@ -1,8 +1,13 @@
 def countdown(n):
-    return n
+    a = -1
+    def lamb():
+        nonlocal a
+        a += 1
+        return n-a
+    return lamb
 
 if __name__ == '__main__':
     n = int(input())
     c = countdown(n)
     for i in range(n):
-        print(c, end=' ')
+        print(c(), end=' ')
