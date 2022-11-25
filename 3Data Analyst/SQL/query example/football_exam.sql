163c17ba4db965fc3a78aac335216743c3a70116
David Kim <rkfka1401@gmail.com>
2022-11-25 10:17:13 +0900

    11/25 study update

--- 3Data Analyst/SQL/query example/football_exam.sql
+++ 3Data Analyst/SQL/query example/football_exam.sql
@@ -31,7 +31,7 @@ from player
 
 select distinct
 	case
-		when position = '' then '신입'
+		when position like '' then '신입'
 		else position
 	end as position
 from player
@@ -53,6 +53,55 @@ where (team_id like
 		(select team_id
 		from team
 		where team_name like ('드래곤즈')))
-and position like 'MF'
-and (height between 170 and 180)
+	and position like 'MF'
+	and (height between 170 and 180)
+;
+
+-- 조인 문제1
+-- 2012년 3월 17일 경기에
+-- 포항 스틸러스 소속 골키퍼(GK)
+-- 선수, 포지션,팀명 (연고지포함),
+-- 스타디움, 경기날짜를 구하시오
+-- 연고지와 팀이름은 간격을 띄우시오(수원[]삼성블루윙즈)
+
+select 
+	p.player_name, p.POSITION,
+	concat(t.region_name, ' ', t.team_name) as reigo_team,
+	st.stadium_name, s.sche_date
+from `SCHEDULE` as s
+	inner join team as t using(stadium_id)
+	inner join player as p using(team_id)
+	inner join stadium as st using(hometeam_id)
+where s.sche_date like '20120317'
+	and t.region_name like '포항'
+	and p.POSITION like 'GK'
+;
+
+-- 조인 문제 2
+-- 포지션이 MF 인 선수들의 소속팀명 및  선수명, 백넘버 출력
+
+select t.team_name, p.player_name, p.back_no
+from player as p
+	inner join team as t using(team_id)
+where p.POSITION like 'MF'
+;
+
+-- 조인 문제 3
+-- 가장 키큰 선수 5명 소속팀명 및  선수명, 백넘버 출력, 
+-- 단 키  값이 없으면 제외
+
+select t.team_name, p.player_name, p.back_no
+from player as p
+	join team as t using(team_id)
+order by height desc
+limit 5
+;
+
+-- 조인 문제 4
+-- 2012년 5월 한달간 경기가 있는 경기장  조회
+
+select st.stadium_name
+from `SCHEDULE` as s
+	join stadium as st using(stadium_id)
+where s.sche_date like '201205%'
 ;
