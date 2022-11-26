7d41d8ebf7e81a3408b7039618b8d96ded0a3616
David Kim <rkfka1401@gmail.com>
2022-11-26 22:48:04 +0900

    11/26 study update

---
+++ 1Python/Dojang/3Programing/unit40_제너레이터.py
@@ -0,0 +1,16 @@
+def prime_number_generator(start, stop):
+    x = []
+    rep = stop - start + 1
+    for i in range(rep):
+        num = start+i
+        x.append(num)
+    yield from x
+
+if __name__ == '__main__':
+    start, stop = map(int, input().split())
+    g = prime_number_generator(start, stop)
+    print(type(g))
+    for i in g:
+        print(i, end= ' ')
+
+# 1 ~ 5 -> 5-1 = 4 1+(0+1)
