8bd6c23f376d26a1fc3c49c7826d146d79e528e3
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2022-12-17 17:34:16 +0900

    12/17 study update

---
+++ 5Flutter/DartBasic/example/1_var.dart
@@ -0,0 +1,44 @@
+void main(){
+  //declare variable
+  var name1 = 'Hi there.';
+  print(name1);
+  var name2 = 'red';
+  print(name2);
+
+  //change
+  name1 = 'Flutter programming';
+  print(name1);
+
+  //declare int
+  int num1 = 2;
+  int num2 = 4;
+  print(num1 + num2);
+  print(num1 - num2);
+  print(num1 / num2);
+  print(num1 * num2);
+  
+  //declare float
+  double num3 = 2.5;
+  double num4 = 0.5;
+  print(num3 + num4);
+  print(num3 - num4);
+  print(num3 / num4);
+  print(num3 * num4);
+
+  //declare boolean
+  bool isTrue = true;
+  bool isFalse = false;
+
+  //declare string
+  String name3 = 'David';
+  String name4 = 'Kim';
+  print(name3 + ' ' + name4);
+  print('${name3} ${name4}');
+
+  //var is automaticly judge what is it.
+
+  //declare dynamic
+  //dynamic type is switchiable.
+  dynamic dyn1 = 'dynamic type.';
+  dynamic dyn2 = 1;
+}
